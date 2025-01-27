f6630e105071a6ed2593a66985636da998a44057
ViniciusDev <viniciusdev10@gmail.com>
2025-01-27 10:02:25 -0300

    created register user with connection api

--- src/pages/register/register.jsx
+++ src/pages/register/register.jsx
@@ -1,7 +1,44 @@
 import "./RegisterPage.css";
 import  Cash from "../../assets/Cash.png"
+import {useRef} from "react"
+import api from "../../services/api";
+import { useNavigate } from "react-router-dom";
 
 function RegisterPage() {
+
+  const inputName = useRef()
+  const inputEmail = useRef()
+  const inputPassword = useRef()
+
+  const navigate = useNavigate()
+
+  async function createdUser(){
+
+    try{
+
+      const name = inputName.current.value
+      const email = inputEmail.current.value
+      const password = inputPassword.current.value
+
+      if(!name || !email || !password){
+        console.error('Name, Email or password is empty');
+        return;
+
+      }
+
+      await api.post('/register', {name,email,password})
+
+      console.log("user created sucessfully")
+      navigate('/login')
+
+    }catch (error){
+
+      console.error('register failed:', error.response?.data || error.message);
+
+    }
+
+  }
+
   return (
     <div className="register-container">
       <div className="branding">
@@ -12,10 +49,10 @@ function RegisterPage() {
       <div className="register-box">
         <h2>Register</h2>
         <form>
-          <input type="text" placeholder="Full Name" required />
-          <input type="email" placeholder="Email" required />
-          <input type="password" placeholder="Password" required />
-          <button type="submit">REGISTER</button>
+          <input type="text" placeholder="Full Name" ref={inputName} required />
+          <input type="email" placeholder="Email" ref={inputEmail} required />
+          <input type="password" placeholder="Password" ref={inputPassword} required />
+          <button type="button" onClick={createdUser}>REGISTER</button>
         </form>
         <a href="/login" className="login-link">Already have an account? Login</a>
       </div>
